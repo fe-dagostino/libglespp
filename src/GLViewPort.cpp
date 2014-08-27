5cf05b588b523a57472ec77aff7c35e8415d55ce
Francesco Emanuele D'Agostino <fedagostino@gmail.com>
2014-08-27 02:54:19 +0200

    update license to lgpl3

--- src/GLViewPort.cpp
+++ src/GLViewPort.cpp
@@ -1,36 +1,39 @@
 /*
-    gles plus plus
-    Copyright (C) 2013 <fedagostino@gmail.com>
+    This file is part of lib gles plus plus.
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
+    libgles++ is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
+    libgles++ is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+    You should have received a copy of the GNU Lesser General Public License
+    along with libgles++.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
 #include "../include/GLViewPort.h"
+#include "../include/GLSceneGraph.h"
 
 
 GLViewPort::GLViewPort( )
-  : m_pSceneGraph( NULL )
+  : m_pSceneGraph( NULL ), m_pos( 0.0f, 0.0f ), m_size( 2, 2 )
 {
   m_matProjection.get() = glm::mat4( 1.0f );
+  
+  reset();
 }
 
 GLViewPort::GLViewPort( GLSceneGraph* pSceneGraph, const glm::mat4& mProjection )
-  : m_pSceneGraph( pSceneGraph )
+  : m_pSceneGraph( pSceneGraph ), m_pos( 0.0f, 0.0f ), m_size( 2, 2 )
 {
   m_matProjection.get() = mProjection;
+  
+  reset();
 }
 
 GLViewPort::~GLViewPort()
@@ -52,21 +55,33 @@ GLSceneGraph* GLViewPort::getScene() const
   return m_pSceneGraph;
 }
 
+VOID GLViewPort::use()
+{
+  glViewport( m_pos.x, m_pos.y, m_size.width, m_size.height );
+}
+
+VOID GLViewPort::reset()
+{
+  GLint viewport[4];	
+  glGetIntegerv(GL_VIEWPORT, viewport);
+  
+  setArea( viewport[0], viewport[1], viewport[2], viewport[3] );
+}
+
 VOID GLViewPort::setArea( GLint x, GLint y, GLsizei width, GLsizei height )
 {
-  glViewport( x, y, width, height );
+  m_pos.x       = x;
+  m_pos.y       = y;
+  m_size.width  = width;
+  m_size.height = height;  
 }
 
 VOID GLViewPort::getArea( GLint& x, GLint& y, GLsizei& width, GLsizei& height ) const
 {
-  GLint viewport[4];
-  
-  glGetIntegerv(GL_VIEWPORT, viewport);
-  
-  x      = viewport[0];
-  y      = viewport[1];
-  width  = viewport[2];
-  height = viewport[3];
+  x      = m_pos.x;
+  y      = m_pos.y;
+  width  = m_size.width;
+  height = m_size.height;
 }
 
 
